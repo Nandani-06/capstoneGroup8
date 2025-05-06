9d5e7f29596bde09e4d8854abc401d130bb4aab9
TroubledPhilip <troubledphilip@gmail.com>
2025-05-06 16:14:37 +0800

    Add dotenv feature and a document for django project start. Update a new gitignore file

--- capstone/quantum_data/quantum_data/settings.py
+++ capstone/quantum_data/quantum_data/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 import os
 from pathlib import Path
 
+from dotenv import load_dotenv
+load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -80,7 +82,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'quantum',  # Your actual database name
         'USER': 'postgres',
-        'PASSWORD': 'admin123',
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD', 'password'),  # Use environment variable for password
         'HOST': 'localhost',
         'PORT': '5432',
     }
