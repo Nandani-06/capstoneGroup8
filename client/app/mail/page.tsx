88df8f3d2a29bb9decf126a64b41102dd5e059ea
TroubledPhilip <troubledphilip@gmail.com>
2025-05-11 20:31:12 +0800

    create a new tab and corresponding page for mailchimp features

---
+++ client/app/mail/page.tsx
@@ -0,0 +1,10 @@
+'use client'
+
+export default function Mailsending() {
+
+  return (
+    <div>
+      Feel free to edit here
+    </div>
+  )
+}
--- client/components/Sidebar.tsx
+++ client/components/Sidebar.tsx
@@ -9,6 +9,8 @@ const navItems = [
   { label: 'Data Upload', href: '/upload' },
   { label: 'Data Preview', href: '/preview' },
   { label: 'Dashboard', href: '/dashboard' },
+  { label: 'Mail sending', href: '/mail' },
+
 ]
 
 export default function Sidebar() {
